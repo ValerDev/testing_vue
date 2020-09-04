5345534ef6b36df948facd4412d6a3a7ff6852ea
Valer <valerghahramanyan@gmail.com>
2020-09-04 11:55:38 +0400

    right image block

--- src/components/background.vue
+++ src/components/background.vue
@@ -1,3 +1,38 @@
 <template>
-  <img src="https://via.placeholder.com/150" />
+<div class="imgBlockContent">
+  <div  class="imgMainBlock" v-if="isImageOpen">
+    <div class="layout"></div>
+    <img src="https://via.placeholder.com/760x320/2200631" />
+    <el-button type="primary" class="detail-button" @click="isImageOpen = !isImageOpen">
+              <img src="../assets/img/icons/plus.svg" alt=""  class=""/>
+              <span>Add Pricing</span>
+    </el-button>
+  </div>
+  <div v-else class="imgMainBlockAfter">
+      <div class="layoutAfter">
+        <div class="layoutButtonBlock">
+          <el-button type="primary" class="detail-button" @click="isImageOpen = !isImageOpen">
+              <span>Idea</span>
+          </el-button>
+          <el-button type="primary" class="detail-button" @click="isImageOpen = !isImageOpen">
+              <span>In development</span>
+          </el-button>
+        </div>
+        <span>10+</span>
+      </div>
+      <img src="https://via.placeholder.com/760x320/2200631" />
+    </div>
+</div>
 </template>
+<script>
+export default {
+    data () {
+    return {
+      isImageOpen: true
+   }
+  }
+}
+</script>
+<style lang='sass'>
+@import '../assets/sass/background/background'
+</style>
